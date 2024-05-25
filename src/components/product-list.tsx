8b8198cb75e81deb73e9c7fc5bd4c586a91c7e11
Nathan-Roberts123 <robertsnathan2004@gmail.com>
2024-05-25 12:29:23 +0200

    updated

--- src/components/product-list.tsx
+++ src/components/product-list.tsx
@@ -15,6 +15,14 @@ const ProductList = () => {
     );
   }
 
+  if (!data) {
+    return (
+      <div className="flex h-full justify-center items-center">
+        No Products Are Available
+      </div>
+    );
+  }
+
   return (
     <div className="relative mx-8 mb-5 pt-4">
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-6 ">
